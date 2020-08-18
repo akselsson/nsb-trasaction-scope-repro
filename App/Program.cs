c9d58e3275cb828cf65435fae3022ac5c6feb964
Patrik Akselsson <pakselsson@paypal.com>
2020-08-18 18:39:05 +0200

    Add handler that writes to sql server but then rolls back

--- App/Program.cs
+++ App/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data;
 using System.Threading.Tasks;
 using System.Transactions;
 using Microsoft.Data.SqlClient;
@@ -15,8 +16,7 @@ namespace App
             rabbitMq.UseConventionalRoutingTopology().ConnectionString("host=localhost");
             endpointConfiguration.EnableInstallers();
             var persistence = endpointConfiguration.UsePersistence<SqlPersistence>();
-            persistence.ConnectionBuilder(() => new SqlConnection(
-                "Data Source=localhost;Initial Catalog=tempdb;User ID=sa;Password=SQL_strong_password_123!"));
+            persistence.ConnectionBuilder(() => new SqlConnection(SqlHelper.ConnectionString));
             persistence.SqlDialect<SqlDialect.MsSqlServer>();
 
             endpointConfiguration.UnitOfWork().WrapHandlersInATransactionScope();
@@ -25,15 +25,23 @@ namespace App
             var startableEndpoint = await Endpoint.Create(endpointConfiguration);
             var endpointInstance = await startableEndpoint.Start();
 
+            await SqlHelper.ExecuteNonQuery(@"IF OBJECT_ID (N'test_table', N'U') IS NOT NULL 
+            drop table test_table;");
+            await SqlHelper.ExecuteNonQuery("create table test_table (id int identity primary key, message_id int)");
+
+            int counter = 0;
             while (true)
             {
-                await endpointInstance.Publish<IExampleEvent>(e => { });
+                await endpointInstance.Publish<IExampleEvent>(e => { e.MessageId = counter++; });
                 await Task.Delay(TimeSpan.FromSeconds(1));
             }
         }
     }
 
-    public interface IExampleEvent : IEvent { }
+    public interface IExampleEvent : IEvent
+    {
+        int MessageId { get; set; }
+    }
 
     public class ExampleEventHandler : IHandleMessages<IExampleEvent>
     {
@@ -49,6 +57,34 @@ namespace App
             }
 
             return Task.CompletedTask;
+        }
+    }
+
+
+    public class SqlHandler : IHandleMessages<IExampleEvent>
+    {
+        public async Task Handle(IExampleEvent message, IMessageHandlerContext context)
+        {
+            await SqlHelper.ExecuteNonQuery($"insert into test_table (message_id) values({message.MessageId})");
+
+            //Uncomment this line to commit the sql insert
+            throw new Exception("This should roll back the previous query");
+        }
+    }
+
+    public static class SqlHelper
+    {
+        public const string ConnectionString = "Data Source=localhost;Initial Catalog=test;User ID=sa;Password=SQL_strong_password_123!";
+
+        public static async Task ExecuteNonQuery(string query)
+        {
+            await using var connection = new SqlConnection(
+                ConnectionString);
+            await connection.OpenAsync();
+            await using var command = connection.CreateCommand();
+            command.CommandText = query;
+            command.CommandType = CommandType.Text;
+            await command.ExecuteNonQueryAsync();
         }
     }
 }
